8a9f9e424813a96ccb6a276634f6e3f559924b5b
Shawn Kim <tkim1@email.arizona.edu>
2024-10-13 23:03:15 -0700

    gin

---
+++ internal/handlers/get_albums.go
@@ -0,0 +1,11 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, Albums)
+}
--- internal/router/router.go
+++ internal/router/router.go
@@ -1,20 +1,15 @@
 package router
 
-import (
-	"net/http"
-
-	"github.com/kimsh02/kay-phos/internal/handlers"
-	"github.com/kimsh02/kay-phos/internal/services"
-)
-
 // initalize routes for all handlers
 func Init() {
 	// init new handler
-	h := handlers.NewHandler()
+	// h := handlers.NewHandler()
 
 	// http.HandleFunc("/", h.HiHandler)
-	http.HandleFunc("/", services.MakeHandler((h.RootHandler)))
-	http.HandleFunc("/view/", services.MakeHandler(h.ViewPageHandler))
-	http.HandleFunc("/edit/", services.MakeHandler(h.EditPageHandler))
-	http.HandleFunc("/save/", services.MakeHandler(h.SavePageHandler))
+
+	// http.HandleFunc("/", services.MakeHandler((h.RootHandler)))
+	// http.HandleFunc("/view/", services.MakeHandler(h.ViewPageHandler))
+	// http.HandleFunc("/edit/", services.MakeHandler(h.EditPageHandler))
+	// http.HandleFunc("/save/", services.MakeHandler(h.SavePageHandler))
+
 }
