9ff7d23838ddc5f5aa6804b98a9bbaf4b8676031
Shawn Kim <tkim1@email.arizona.edu>
2024-12-26 01:26:10 -0700

    update

--- server/basic/internal/router/router.go
+++ server/basic/internal/router/router.go
@@ -13,7 +13,6 @@ func Init() {
 	h := handlers.NewHandler()
 
 	// http.HandleFunc("/", h.HiHandler)
-
 	http.HandleFunc("/", services.MakeHandler((h.RootHandler)))
 	http.HandleFunc("/view/", services.MakeHandler(h.ViewPageHandler))
 	http.HandleFunc("/edit/", services.MakeHandler(h.EditPageHandler))
---
+++ server/gin/app.go
@@ -0,0 +1,11 @@
+// package main
+
+// import "github.com/gin-gonic/gin"
+
+// func main() {
+
+// 	// gin framework server start and run
+// 	r := gin.Default()
+// 	// default port 8080
+// 	r.Run()
+// }
