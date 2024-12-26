e2276d28336fa90f97ea572da638b6a3e4e62693
Shawn Kim <tkim1@email.arizona.edu>
2024-12-26 01:34:21 -0700

    update

--- server/basic/cmd/server/main.go
+++ server/basic/cmd/server/main.go
@@ -11,7 +11,7 @@ func main() {
 	// set all routes for all handlers
 	router.Init()
 
-	// startup server
+	// startup the server
 	log.Println("Starting server on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
