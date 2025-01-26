96aba309a36cefd8e931c7357b5d6080870e4994
Shawn Kim <tkim1@email.arizona.edu>
2025-01-25 23:16:13 -0700

    update

--- database/startup/docker_db.sh
+++ database/startup/docker_db.sh
@@ -20,19 +20,14 @@ fi
 
 # convert xlsx to csv
 if [ ! -f "fndds_data\fndds_nutrient_values.csv " ]; then
-    xlsx2csv fndds_data/2021-2023%20FNDDS%20At%20A%20Glance%20-%20FNDDS%20Nutrient%20Values.xlsx fndds_data/fndds_nutrient_values.csv
+    xlsx2csv fndds_data/fndds_data/2021-2023.xlsx fndds_data/fndds_nutrient_values.csv
 fi
 
 # remove title lines from csv file
 sed -i '1,2d' fndds_data/fndds_nutrient_values.csv
 
-# drop all relations in db
-# psql -d kayphos -f sql_scripts/drop.sql
-
 # import csv to postgres
-# TODO: CHANGE STRING TO YOUR POSTGRES DB
-# USER=$(whoami)
-csvsql --insert --db "postgresql://postgres:localhost@localhost:5432/kayphos" fndds_data/fndds_nutrient_values.csv
+csvsql --insert --db "postgresql://$PGUSER:$PGPASSWORD@$PGHOST:5432/kayphos" fndds_data/fndds_nutrient_values.csv
 
 # create new column with GIN index on "Main food description" column
 psql -d kayphos -U postgres -f sql_scripts/gin_index.sql
