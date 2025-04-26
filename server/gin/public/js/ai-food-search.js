3c389accd7dee388853b5910fa1b277e4b22955c
PerlaElizabeth <145716971+PerlaElizabeth@users.noreply.github.com>
2025-04-26 11:35:01 -0700

    Added Hamburger menu, change name from user define meal to favorites

--- server/gin/public/js/ai-food-search.js
+++ server/gin/public/js/ai-food-search.js
@@ -583,4 +583,12 @@ if (typeof module !== "undefined" && module.exports) {
     };
 }
 
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', function() {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+});
+
 
--- server/gin/public/js/manual-food-search.js
+++ server/gin/public/js/manual-food-search.js
@@ -1,3 +1,11 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', function() {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+});
+
 if (typeof window !== "undefined") {
   selectedFoods = window.selectedFoods || [];
   analysisResults = window.analysisResults || [];
